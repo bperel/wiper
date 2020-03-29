0d67ca470c944ff955c9aa63144dd741f64f00d2
Bruno Perel <brunoperel@gmail.com>
2020-03-29 15:11:24 +0200

    Move server-related files to languagetool

--- scripts/entrypoint-languagetool-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-basepath=/srv/languagetool-runtime
-languagetool_path=${basepath}/`ls ${basepath}`
-java -cp ${languagetool_path}/'*' org.languagetool.server.HTTPServer --port 8010 --public --allow-origin '*' --config /home/server.properties
