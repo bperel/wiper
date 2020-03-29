d38015bb14992b6c7cfb128679076fcac2d12654
Bruno Perel <brunoperel@gmail.com>
2020-03-29 16:25:02 +0200

    Add alpha mention

--- src/views/Navbar.vue
+++ src/views/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">WiPeR</a>
+    <a class="navbar-brand" href="#">WiPeR<sup>alpha</sup></a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto"></ul>
       <button
